80c349dd3d376fb0ae46c4641912ebf89e551de8
JOshi kv <joshikv3705@gmail.com>
2023-04-04 09:59:10 +0530

    login form created

--- chatapp/User/views.py
+++ chatapp/User/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-@login_required(login_url='User:signup')
+
 def signup(request):
     return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
