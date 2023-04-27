51967ee448fd278cc307515ca020d57bf49d3196
JOshi kv <joshikv3705@gmail.com>
2023-04-27 11:55:10 +0530

    updating offline status

--- chatapp/static/js/online_status.js
+++ chatapp/static/js/online_status.js
@@ -3,6 +3,7 @@ let onlineLocation = window.location
 let onlineWsStart = 'ws://'
 
 let loggedinUser = JSON.parse(document.getElementById('json-message-username').textContent)
+let otherUsername = JSON.parse(document.getElementById('json-other-username').textContent)
 
 if (onlineLocation.protocol === 'https') {
     let onlineWsStart = 'wss://'
@@ -22,8 +23,21 @@ onlineStatus.onopen = function (e) {
     })) 
 }
 
+window.addEventListener('beforeunload',function(e){
+    onlineStatus.send(JSON.stringify({
+        'username' : loggedinUser,
+        'type' : 'offline'
+    }))
+})
+
 onlineStatus.onmessage = function(e) {
     console.log(e);
+    let data = JSON.parse(e.data)
+    if(data.username !== loggedinUser){
+        let onlineImage = document.getElementById(`${data.username}-image`)
+        console.log(onlineImage)
+    }
+
 }
 
 onlineStatus.onclose = function (e) {
