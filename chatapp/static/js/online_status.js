3e4d91b752931b7b641192e11c250a1a3bbf8ab9
JOshi kv <joshikv3705@gmail.com>
2023-04-28 10:06:27 +0530

    updating online/offline status without page reload

--- chatapp/static/js/online_status.js
+++ chatapp/static/js/online_status.js
@@ -3,7 +3,7 @@ let onlineLocation = window.location
 let onlineWsStart = 'ws://'
 
 let loggedinUser = JSON.parse(document.getElementById('json-message-username').textContent)
-let otherUsername = JSON.parse(document.getElementById('json-other-username').textContent)
+
 
 if (onlineLocation.protocol === 'https') {
     let onlineWsStart = 'wss://'
@@ -35,7 +35,21 @@ onlineStatus.onmessage = function(e) {
     let data = JSON.parse(e.data)
     if(data.username !== loggedinUser){
         let onlineImage = document.getElementById(`${data.username}-image`)
-        console.log(onlineImage)
+        let onlineCaption = document.getElementById(`${data.username}-caption`)
+
+        if(data.online_status == true){
+            onlineImage.classList.remove('offline')
+            onlineCaption.classList.remove('offline-caption')
+            onlineImage.classList.add('online')
+            onlineCaption.classList.add('online-caption')
+
+        }else{
+            onlineImage.classList.remove('online')
+            onlineCaption.classList.remove('online-caption')
+            onlineImage.classList.add('offline')
+            onlineCaption.classList.add('offline-caption')
+        }
+       
     }
 
 }
